82ca97ba7230fce710e68510b88ada4bb6273a14
Gustavo Moraes <gustavosotnas1@gmail.com>
2015-10-12 00:01:05 -0300

    Substituído o texto "Pesquisar" para um botão com imagem de lupa.

--- js/script.js
+++ js/script.js
@@ -85,7 +85,9 @@ function updateClock() {
 function searchBox(url, name, placeholder) {
 	var string = '<form method="get" action="' + url + '">'
 	           + '<input type="text" id="g" name="' + name + '" placeholder="' + placeholder + '" maxlength="255" value="">'
-	           + '<input type="submit" value="Pesquisar">'
+	           + '<button type="submit">'
+	           + '<img src="./font/magnifying49.svg" height="16" width="16">'
+	           + '</button>'
 	           + '</form>';
 	return string;
 }
